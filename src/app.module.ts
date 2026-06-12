280888c55945405b571a46696cd8dbd59f8787a0
Eniola3321 <ekanadeadeniyiabdulhafis@gmail.com>
2026-06-12 10:30:00 +0100

    all fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './prisma/prisma.service';
 import { UserModule } from './user/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { BusinessModule } from './business/business.module';
+import { TransactionModule } from './transaction/transaction.module';
+import { WalletModule } from './wallet/wallet.module';
+import { FxRateModule } from './fx-rate/fx-rate.module';
+import { InvoiceModule } from './invoice/invoice.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule, BusinessModule, TransactionModule, WalletModule, FxRateModule, InvoiceModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
