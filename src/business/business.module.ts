568a4d00eaf3cf0161af654ef2e4f318c8bb2dc8
Treasure332 <treasureandcryptohunt@gmail.com>
2026-06-19 14:54:28 +0100

    feat: issues solved

--- src/business/business.module.ts
+++ src/business/business.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { BusinessService } from './business.service';
 import { BusinessController } from './business.controller';
+import { BusinessService } from './business.service';
 
 @Module({
   providers: [BusinessService],
